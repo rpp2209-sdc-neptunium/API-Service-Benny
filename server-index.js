1fdc5f890eb869c2982986e32b58bccd7d0db501
bennyv8 <bvanhr@gmail.com>
2023-04-05 23:27:42 -0700

    added static files path

--- server-index.js
+++ server-index.js
@@ -4,13 +4,14 @@ const app = express();
 const port = process.env.PORT || 8080;
 const router = require('./routes.js');
 const logger = require('./logger.js');
-
+const path = require('path')
 
 // =============================================
 //                Middleware
 // =============================================
 app.use(express.json());
 app.use(logger);
+app.use(express.static(path.join(__dirname, 'dist')));
 
 // =============================================
 //               Route Imports
